e5e6c5e55c292af8d4e674f29c8acf96aefbd22d
rodrikraus <rodrikraus10@hotmail.com>
2023-04-18 23:17:19 -0300

    Agregadas las constantes para los Strings

--- app/src/main/java/ayds/newyork/songinfo/home/view/ReleaseDateConverterInjector.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/ReleaseDateConverterInjector.kt
@@ -1,13 +1,18 @@
 package ayds.newyork.songinfo.home.view
 
 object ReleaseDateConverterInjector{
+    private const val DAY_PRECISION = "day"
+    private const val MONTH_PRECISION = "month"
+    private const val YEAR_PRECISION = "year"
+
     fun getConverter(songReleaseDatePrecision: String) : ReleaseDateFormatStrategy{
         val releaseDateConverterFormat : ReleaseDateFormatStrategy = when(songReleaseDatePrecision){
-            "day" ->  ReleaseDateFormatDayStrategy()
-            "month" -> ReleaseDateFormatMonthStrategy()
-            "year" ->  ReleaseDateFormatYearStrategy()
+            DAY_PRECISION -> ReleaseDateFormatDayStrategy()
+            MONTH_PRECISION -> ReleaseDateFormatMonthStrategy()
+            YEAR_PRECISION -> ReleaseDateFormatYearStrategy()
             else -> ReleaseDateFormatDefaultStrategy()
         }
         return releaseDateConverterFormat
     }
 }
+
