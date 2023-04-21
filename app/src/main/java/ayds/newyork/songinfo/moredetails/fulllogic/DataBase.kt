b1c471f723f149a8ade1ed7d6ef028db2446e5a7
VirgilioOC <VirgilioocamposUNS@gmail.com>
2023-04-21 16:37:59 -0300

    Refactor getInfo

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
@@ -2,6 +2,7 @@ package ayds.newyork.songinfo.moredetails.fulllogic
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
@@ -20,7 +21,7 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, "dictionary.db", nu
         this.writableDatabase.insert("artists", null, createArtistWithValues(artist, info))
 
 
-    private fun createArtistWithValues(artist: String, info: String): ContentValues{
+    private fun createArtistWithValues(artist: String, info: String): ContentValues {
         val values = ContentValues()
 
         values.put("artist", artist)
@@ -30,42 +31,37 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, "dictionary.db", nu
         return values
     }
 
-    @JvmStatic
-    fun getInfo(dbHelper: DataBase, artist: String): String? {
-        val db = dbHelper.readableDatabase
+    fun DataBase.getInfo(artist: String): String? {
+        val items = cursorIterator(createCursor(this, artist))
+        return if (items.isEmpty()) null else items[0]
+    }
 
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
-        val projection = arrayOf(
-            "id",
-            "artist",
-            "info"
+    private fun createCursor(dbHelper: DataBase, artist: String): Cursor {
+        return dbHelper.readableDatabase.query(
+            "artists",
+            constructProjection(),
+            "artist  = ?",
+            arrayOf(artist),
+            null,
+            null,
+            "artist DESC"
         )
+    }
 
-// Filter results WHERE "title" = 'My Title'
-        val selection = "artist  = ?"
-        val selectionArgs = arrayOf(artist)
+    private fun constructProjection() = arrayOf("id", "artist", "info")
 
-// How you want the results sorted in the resulting Cursor
-        val sortOrder = "artist DESC"
-        val cursor = db.query(
-            "artists",  // The table to query
-            projection,  // The array of columns to return (pass null to get all)
-            selection,  // The columns for the WHERE clause
-            selectionArgs,  // The values for the WHERE clause
-            null,  // don't group the rows
-            null,  // don't filter by row groups
-            sortOrder // The sort order
-        )
+    private fun cursorIterator(cursor: Cursor): MutableList<String>{
         val items: MutableList<String> = ArrayList()
+
         while (cursor.moveToNext()) {
             val info = cursor.getString(
                 cursor.getColumnIndexOrThrow("info")
             )
             items.add(info)
         }
-        cursor.close()
-        return if (items.isEmpty()) null else items[0]
-    }
 
+        cursor.close()
+
+        return items
+    }
 }
