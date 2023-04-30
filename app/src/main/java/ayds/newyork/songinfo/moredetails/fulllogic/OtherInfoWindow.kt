2e71328b1a1b527cb4c23e9dcbdb0034672e1314
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-04-30 14:01:47 -0300

    Refactor: agrego constantes y acomodo funciones

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,5 +1,4 @@
 package ayds.newyork.songinfo.moredetails.fulllogic
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -17,7 +16,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.IOException
 import java.util.*
-
 private const val IN_LOCAL_REPOSITORY = "[*]"
 const val ARTIST_NAME = "artistName"
 private const val LINK_API_NYTIMES = "https://api.nytimes.com/svc/search/v2/"
@@ -25,38 +23,46 @@ private const val HTML_DIV_WIDTH = "400"
 private const val HTML_FONT_FACE = "arial"
 private const val IMAGE_URL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
-
+private const val PROP_RESPONSE = "response"
+private const val OPEN_LABEL_HTML = "<html>"
+private const val OPEN_DIV_WIDTH = "<div width="
+private const val OPEN_FONT_FACE = "<font face="
+private const val CLOSE_LABEL = ">"
+private const val LINE_JUMP_HTML = "<br>"
+private const val OPEN_LABEL_WORD_BLACK = "<b>"
+private const val CLOSE_LABEL_WORD_BLACK = "</b>"
+private const val CLOSE_LABEL_HTML = "</html>"
+private const val CLOSE_LABEL_DIV = "</div>"
+private const val CLOSE_LABEL_FONT = "</font>"
 class OtherInfoWindow : AppCompatActivity() {
-
     private lateinit var textInfoWindow: TextView
     private lateinit var dataBase: DataBase
     private val retrofit = createRetroFit()
     private val nyTimesAPI = createAPI(retrofit)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_info)
         initViewInfo()
         initDataBase()
         val artistName = intent.getStringExtra(ARTIST_NAME)
         loadArtistInfo(artistName)
     }
-
     private fun initDataBase() {
         dataBase = DataBase(this)
     }
-
     private fun initViewInfo() {
-        setContentView(R.layout.activity_other_info)
-        textInfoWindow = findViewById(R.id.textPane2)
+        textInfoWindow = findViewById(R.id.textInfo)
     }
-
     private fun generateResponse(nyTimesAPI: NYTimesAPI, artistName: String?): JsonObject {
-        val callResponse: Response<String> = nyTimesAPI.getArtistInfo(artistName).execute()
-        val gson = Gson()
-        val jObj = gson.fromJson(callResponse.body(), JsonObject::class.java)
-        return jObj["response"].asJsonObject
+        val callResponse = getResponse(nyTimesAPI, artistName)
+        val jObj = getJson(callResponse)
+        return jObj[PROP_RESPONSE].asJsonObject
     }
-
+    private fun getResponse(nyTimesAPI: NYTimesAPI, artistName: String?) = nyTimesAPI.getArtistInfo(artistName).execute()
+    private fun getJson(callResponse : Response<String>): JsonObject {
+        val gson = Gson()
+        return gson.fromJson(callResponse.body(), JsonObject::class.java)
+    }
     private fun loadArtistInfo(artistName: String?) {
         Thread {
             var infoArtista: String? = dataBase.getInfo(artistName)
@@ -113,15 +119,15 @@ class OtherInfoWindow : AppCompatActivity() {
 
     private fun textToHtml(text: String, term: String?): String {
         return with(StringBuilder()) {
-            append("<html>")
-            append("<div width=").append(HTML_DIV_WIDTH).append(">")
-            append("<font face=").append(HTML_FONT_FACE).append(">")
+            append(OPEN_LABEL_HTML)
+            append(OPEN_DIV_WIDTH).append(HTML_DIV_WIDTH).append(CLOSE_LABEL)
+            append(OPEN_FONT_FACE).append(HTML_FONT_FACE).append(CLOSE_LABEL)
             val textWithBold = text
                 .replace("'", " ")
-                .replace("\n", "<br>")
-                .replace("(?i)$term".toRegex(), "<b>" + term!!.uppercase(Locale.getDefault()) + "</b>")
+                .replace("\n", LINE_JUMP_HTML)
+                .replace("(?i)$term".toRegex(), OPEN_LABEL_WORD_BLACK + term!!.uppercase(Locale.getDefault()) + CLOSE_LABEL_WORD_BLACK)
             append(textWithBold)
-            append("</font></div></html>")
+            append("$CLOSE_LABEL_FONT$CLOSE_LABEL_DIV$CLOSE_LABEL_HTML")
             toString()
         }
     }
