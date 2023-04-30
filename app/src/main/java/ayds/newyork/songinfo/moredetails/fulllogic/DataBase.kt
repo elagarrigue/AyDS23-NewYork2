e711679eddaaa50d8695710f19a16ccd925e4b36
Juan Cruz Rossi <juanrossi1969@gmail.com>
2023-04-30 13:20:07 -0300

    Creación constantes en DataBase

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
@@ -5,33 +5,36 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.util.Log
 
-class DataBase(context: Context) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
-    private val TABLE = "artist"
-    private val PROJECTION = arrayOf("id", "artist", "info")
-    private val SELECTION = "artist  = ?"
-    private val ORDER = "artist DESC"
+private const val DATABASE_NAME = "dictionary.db"
+private const val ARTISTS_TABLE_NAME = "artists"
+private const val COLUMN_ID = "id"
+private const val COLUMN_ARTIST = "artist"
+private const val COLUMN_ARTIST_INFO = "info"
+private const val COLUMN_SOURCE = "source"
+private const val SOURCE_VALUE = 1
+private const val ARTIST_TABLE_CREATION_QUERY = "create table $ARTISTS_TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMN_ARTIST string, $COLUMN_ARTIST_INFO string, $COLUMN_SOURCE integer)"
+private const val SELECTION = "$COLUMN_ARTIST  = ?"
+private const val ORDER = "$COLUMN_ARTIST DESC"
 
+class DataBase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(
-            "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
-        )
+        db.execSQL(ARTIST_TABLE_CREATION_QUERY)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
     fun saveArtist(artist: String?, info: String) =
-        this.writableDatabase.insert("artists", null, createArtistWithValues(artist, info))
+        this.writableDatabase.insert(ARTISTS_TABLE_NAME, null, createArtistWithValues(artist, info))
 
 
     private fun createArtistWithValues(artist: String?, info: String): ContentValues {
         val values = ContentValues()
 
         with(values){
-            put("artist", artist)
-            put("info", info)
-            put("source", 1)
+            put(COLUMN_ARTIST, artist)
+            put(COLUMN_ARTIST_INFO, info)
+            put(COLUMN_SOURCE, SOURCE_VALUE)
         }
         return values
     }
@@ -43,8 +46,8 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, "dictionary.db", nu
 
     private fun createCursor(dbHelper: DataBase, artist: String?): Cursor {
         return dbHelper.readableDatabase.query(
-            TABLE,
-            PROJECTION,
+            ARTISTS_TABLE_NAME,
+            arrayOf(COLUMN_ID, COLUMN_ARTIST, COLUMN_ARTIST_INFO),
             SELECTION,
             arrayOf(artist),
             null,
@@ -57,7 +60,7 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, "dictionary.db", nu
         val items: MutableList<String> = ArrayList()
         while (cursor.moveToNext()) {
             val info = cursor.getString(
-                cursor.getColumnIndexOrThrow("info")
+                cursor.getColumnIndexOrThrow(COLUMN_ARTIST_INFO)
             )
             items.add(info)
         }
