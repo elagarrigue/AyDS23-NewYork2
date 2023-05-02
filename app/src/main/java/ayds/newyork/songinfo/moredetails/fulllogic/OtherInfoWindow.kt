f34d288e68ce1c3c636fd670333e15116541a08a
rodrikraus <rodrikraus10@hotmail.com>
2023-05-02 17:40:11 -0300

    Fix indentacion y abstraccion de onCreate.

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,4 +1,5 @@
 package ayds.newyork.songinfo.moredetails.fulllogic
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -17,6 +18,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.IOException
 import java.util.*
+
 private const val IN_LOCAL_REPOSITORY = "[*]"
 const val ARTIST_NAME = "artistName"
 private const val LINK_API_NYTIMES = "https://api.nytimes.com/svc/search/v2/"
@@ -38,41 +40,51 @@ private const val CLOSE_LABEL_FONT = "</font>"
 private const val NO_RESULTS = "No Results"
 private const val WEB_URL = "web_url"
 private const val DOCS = "docs"
+
 class OtherInfoWindow : AppCompatActivity() {
     private lateinit var textInfoWindow: TextView
     private lateinit var dataBase: DataBase
     private val retrofit = createRetroFit()
     private val nyTimesAPI = createAPI(retrofit)
+    private val artistName = intent.getStringExtra(ARTIST_NAME)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_info)
         initViewInfo()
         initDataBase()
-        val artistName = intent.getStringExtra(ARTIST_NAME)
         loadArtistInfo(artistName)
     }
+
     private fun initDataBase() {
         dataBase = DataBase(this)
     }
+
     private fun initViewInfo() {
         textInfoWindow = findViewById(R.id.textInfo)
     }
+
     private fun generateResponse(nyTimesAPI: NYTimesAPI, artistName: String?): JsonObject {
         val callResponse = getResponse(nyTimesAPI, artistName)
         val jObj = getJson(callResponse)
         return jObj[PROP_RESPONSE].asJsonObject
     }
-    private fun getResponse(nyTimesAPI: NYTimesAPI, artistName: String?) = nyTimesAPI.getArtistInfo(artistName).execute()
-    private fun getJson(callResponse : Response<String>): JsonObject {
+
+    private fun getResponse(nyTimesAPI: NYTimesAPI, artistName: String?) =
+        nyTimesAPI.getArtistInfo(artistName).execute()
+
+    private fun getJson(callResponse: Response<String>): JsonObject {
         val gson = Gson()
         return gson.fromJson(callResponse.body(), JsonObject::class.java)
     }
+
     private fun loadArtistInfo(artistName: String?) {
         Thread {
             repositoryImplementation(artistName)
         }.start()
     }
-    private fun repositoryImplementation(artistName: String?){
+
+    private fun repositoryImplementation(artistName: String?) {
         var infoArtist: String? = getInfoDataBase(artistName)
         if (infoArtist != null) {
             infoArtist = "$IN_LOCAL_REPOSITORY$infoArtist"
@@ -94,12 +106,17 @@ class OtherInfoWindow : AppCompatActivity() {
         }
         setImage(infoArtist)
     }
-    private fun updateInfoArtist(abstract: JsonElement, artistName: String?) : String{
+
+    private fun updateInfoArtist(abstract: JsonElement, artistName: String?): String {
         val infoArtist = abstract.asString.replace("\\n", "\n")
         return textToHtml(infoArtist, artistName)
     }
-    private fun getAsJsonObject(response: JsonObject) = response["docs"].asJsonArray[0].asJsonObject["abstract"]
+
+    private fun getAsJsonObject(response: JsonObject) =
+        response["docs"].asJsonArray[0].asJsonObject["abstract"]
+
     private fun getInfoDataBase(artistName: String?) = dataBase.getInfo(artistName)
+
     @Suppress("DEPRECATION")
     private fun setImage(infoArtist: String?) {
         runOnUiThread {
@@ -108,13 +125,16 @@ class OtherInfoWindow : AppCompatActivity() {
             textInfoWindow.text = Html.fromHtml(infoArtist)
         }
     }
+
     private fun saveArtistOnDatabase(artistName: String?, infoArtist: String) {
         dataBase.saveArtist(artistName, infoArtist)
     }
+
     private fun getURL(infoArtist: String?): String {
         val response = generateResponse(nyTimesAPI, infoArtist)
         return response[DOCS].asJsonArray[0].asJsonObject[WEB_URL].asString
     }
+
     private fun setButtonClickListener(urlString: String) {
         findViewById<View>(R.id.openUrlButton).setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
@@ -122,11 +142,13 @@ class OtherInfoWindow : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
     private fun createAPI(retrofit: Retrofit): NYTimesAPI = retrofit.create(NYTimesAPI::class.java)
     private fun createRetroFit(): Retrofit = Retrofit.Builder()
         .baseUrl(LINK_API_NYTIMES)
         .addConverterFactory(ScalarsConverterFactory.create())
         .build()
+
     private fun textToHtml(text: String, term: String?): String {
         return with(StringBuilder()) {
             append(OPEN_LABEL_HTML)
@@ -135,7 +157,10 @@ class OtherInfoWindow : AppCompatActivity() {
             val textWithBold = text
                 .replace("'", " ")
                 .replace("\n", LINE_JUMP_HTML)
-                .replace("(?i)$term".toRegex(), OPEN_LABEL_WORD_BLACK + term!!.uppercase(Locale.getDefault()) + CLOSE_LABEL_WORD_BLACK)
+                .replace(
+                    "(?i)$term".toRegex(),
+                    OPEN_LABEL_WORD_BLACK + term!!.uppercase(Locale.getDefault()) + CLOSE_LABEL_WORD_BLACK
+                )
             append(textWithBold)
             append("$CLOSE_LABEL_FONT$CLOSE_LABEL_DIV$CLOSE_LABEL_HTML")
             toString()
