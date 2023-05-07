0f1b100e98ea2b7e1c79c8ab846270ca7763e333
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-05-06 22:32:06 -0300

    Implementacion de Injector de API

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/nytimes/service/NYTimesServiceInjector.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/nytimes/service/NYTimesServiceInjector.kt
@@ -1,5 +1,21 @@
 package ayds.newyork.songinfo.moredetails.model.repository.external.nytimes.service
 
+import ayds.newyork.songinfo.moredetails.model.repository.external.nytimes.NYTimesAPI
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 object NYTimesServiceInjector {
-    //aca se deberia hacer el create api ej:spotify track injector y lo del retrofit
+    private const val LINK_API_NYTIMES = "https://api.nytimes.com/svc/search/v2/"
+
+    private val nyTimesAPIRetrofit = Retrofit.Builder()
+        .baseUrl(LINK_API_NYTIMES)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+    private val nyTimesAPI = nyTimesAPIRetrofit.create(NYTimesAPI::class.java)
+    private val nyTimesToArtistResolver: NYTimesToArtistResolver = JsonToArtistResolver()
+
+    val nyTimesService: NYTimesService = NYTimesServiceImpl(
+        nyTimesAPI,
+        nyTimesToArtistResolver
+    )
 }
