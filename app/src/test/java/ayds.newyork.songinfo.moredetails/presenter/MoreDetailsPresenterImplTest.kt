07943400104f7d0c6db290393b64acea60a605f5
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-05-15 23:41:40 -0300

    Test Presenter

--- app/src/test/java/ayds.newyork.songinfo.moredetails/presenter/MoreDetailsPresenterImplTest.kt
+++ app/src/test/java/ayds.newyork.songinfo.moredetails/presenter/MoreDetailsPresenterImplTest.kt
@@ -1,48 +1,67 @@
-package ayds.newyork.songinfo.moredetails.presenter
+package ayds.newyork.songinfo.moredetails.presentation.presenter
 
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistData
-import ayds.newyork.songinfo.moredetails.domain.repository.ArtistRepository
-import ayds.newyork.songinfo.moredetails.presentation.presenter.MoreDetailsPresenterImpl
 import ayds.newyork.songinfo.moredetails.presentation.MoreDetailsUIState
-import ayds.newyork.songinfo.moredetails.presentation.presenter.RepositoryToViewFormatter
-import ayds.observer.Observer
 import ayds.observer.Subject
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import ayds.newyork.songinfo.moredetails.domain.repository.ArtistRepository as ArtistRepository
 
-class MoreDetailsPresenterImplTest {
+class MoreDetailsPresenterTest {
 
-    private val artistRepository: ArtistRepository = mockk()
-    private val repositoryToViewFormatter: RepositoryToViewFormatter = mockk()
-
-    private val uiStateObserver: Observer<MoreDetailsUIState> = mockk()
-    private val onUIStateSubject = Subject<MoreDetailsUIState>()
-    private val moreDetailsPresenter: MoreDetailsPresenterImpl =
-        MoreDetailsPresenterImpl(artistRepository, repositoryToViewFormatter).apply {
-            uiStateObservable.subscribe(uiStateObserver)
-        }
-
-
+    private lateinit var presenter: MoreDetailsPresenter
+    private val repositoryMock: ArtistRepository = mockk()
+    private val formatterMock: RepositoryToViewFormatter = mockk()
+    private val uiStateSubject: Subject<MoreDetailsUIState> = Subject()
 
     @Before
-    fun setup() {
-        every { onUIStateSubject.notify(any()) } returns Unit
+    fun setUp() {
+        presenter = MoreDetailsPresenterImpl(repositoryMock, formatterMock)
+        every { formatterMock.format(any()) } returns "[*]formato"
+        val artistData: ArtistData = mockk()
+        every { repositoryMock.getArtistData(any()) } returns artistData
     }
 
     @Test
-    fun `openArtistInfoWindow should load artist info and notify UI state observer`() {
+    fun `openArtistInfoWindow should update the UI state with the formatted data`() {
         val artistName = "Radiohead"
-        val artistData: ArtistData = mockk()
-        val expectedUiState = MoreDetailsUIState("formatted data", "https://example.com")
+        presenter.openArtistInfoWindow(artistName)
+        Thread.sleep(10000)
 
-        every { artistRepository.getArtistData(artistName) } returns artistData
-        every { repositoryToViewFormatter.format(artistData) } returns expectedUiState.info
+        val artistData = ArtistData.ArtistWithData("Radiohead","info" , "url", true)
+        val expectedFormattedData = "[*]formato"
+        every { repositoryMock.getArtistData(artistName) } returns artistData
+        every { formatterMock.format(artistData) } returns expectedFormattedData
 
-        moreDetailsPresenter.openArtistInfoWindow(artistName)
+        val expectedUIState = MoreDetailsUIState(
+            expectedFormattedData,
+            artistData.url,
+            "imagen")
 
-        verify { onUIStateSubject.notify(expectedUiState) }
+        verify { formatterMock.format(artistData) }
+        val actualUiState = uiStateSubject.lastValue()
+
+        assertEquals(expectedUIState, actualUiState)
+        assertEquals("imagen",actualUiState?.urlImagen)
+        assertEquals("info",actualUiState?.info)
+        assertEquals("url", actualUiState?.url)
+    }
+
+    @Test
+    fun `loadArtistInfo should update the UI state with no results if the artist data is null`() {
+
+        val artistName = ""
+        val expectedFormattedData = "No Results"
+
+        presenter.openArtistInfoWindow(artistName)
+
+        val actualUiState = uiStateSubject.lastValue()
+
+        assertEquals(expectedFormattedData, actualUiState)
+        assertEquals("", actualUiState?.url)
     }
 }
