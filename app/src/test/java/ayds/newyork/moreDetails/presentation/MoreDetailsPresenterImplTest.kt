a99fe959ee228025ba13ee78146e8ca388c1c484
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-05-20 15:29:08 -0300

    Correccion test Presenter

--- app/src/test/java/ayds/newyork/moreDetails/presentation/MoreDetailsPresenterImplTest.kt
+++ app/src/test/java/ayds/newyork/moreDetails/presentation/MoreDetailsPresenterImplTest.kt
@@ -5,6 +5,7 @@ import ayds.newyork.songinfo.moredetails.presentation.MoreDetailsUIState
 import ayds.observer.Subject
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -21,18 +22,15 @@ class MoreDetailsPresenterTest {
     fun setUp() {
         presenter = MoreDetailsPresenterImpl(repositoryMock, formatterMock)
         uiStateSubject = presenter.onUIStateSubject
-        every { formatterMock.format(any()) } returns "info"
-        val artistData: ArtistData = mockk()
-        every { repositoryMock.getArtistData(any()) } returns artistData
     }
 
     @Test
     fun `openArtistInfoWindow should update the UI state with the formatted data`() {
         val artistName = "Radiohead"
         val artistData = ArtistData.ArtistWithData("Radiohead","info" , "url", true)
-        val expectedInfo = "info"
+        val expectedInfo = "[*]info"
         val expectedUIState = MoreDetailsUIState(
-            artistData.info,
+            expectedInfo,
             artistData.url,
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU")
         every { repositoryMock.getArtistData(artistName) } returns artistData
@@ -46,17 +44,17 @@ class MoreDetailsPresenterTest {
     }
 
     @Test
-    fun `loadArtistInfo should update the UI state with no results if the artist data is null`() {
-        val artistName = ""
-        val expectedUIState = MoreDetailsUIState(
-            "info",
-            "",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU")
+    fun `on search artist it should notify the result`() {
+        val uiState: MoreDetailsUIState = presenter.uiState
+        val artistData: ArtistData = mockk()
+        every { repositoryMock.getArtistData("artistName") } returns artistData
+        val observableTester: (MoreDetailsUIState) -> Unit = mockk(relaxed = true)
+        presenter.uiStateObservable.subscribe {
+            observableTester(it)
+        }
 
-        presenter.openArtistInfoWindow(artistName)
-        Thread.sleep(20000)
+        presenter.openArtistInfoWindow("artistName")
 
-        val actualUiState = uiStateSubject.lastValue()
-        assertEquals(expectedUIState, actualUiState)
+        verify { observableTester(uiState) }
     }
 }
